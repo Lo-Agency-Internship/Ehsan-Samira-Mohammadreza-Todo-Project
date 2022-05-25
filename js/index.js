779fbb07b88195af68125a3f020e84e4a42e01ad
ehsan-afsharnia <iamafsharnia@gmail.com>
2022-05-25 20:35:52 +0430

    Fix register validation, Add login function, Format codes

--- js/index.js
+++ js/index.js
@@ -1,72 +1,85 @@
-const form = document.querySelector('#login-account-form');
-const emailInput = document.querySelector('#email');
-const passwordInput = document.querySelector('#password');
+const form = document.querySelector("#login-account-form");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
 
-form.addEventListener('submit', (event) => {
-
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  let valid = validateForm()
-  console.log(valid)
-    if (!valid) {
-
-      return false
-    }
-
-
-
-
-
-
-
-
-
+  let valid = validateForm();
+  console.log(valid);
+  if (!valid) {
+    return false;
+  }
+  canLogin();
 });
 
-
-
 function validateForm() {
-
   //EMAIL
-  if (emailInput.value.trim() == '') {
-    return setError(emailInput, 'Provide email address');
-
+  if (emailInput.value.trim() == "") {
+    return setError(emailInput, "Provide email address");
   } else if (isEmailValid(emailInput.value)) {
     return setSuccess(emailInput);
   } else {
-     setError(emailInput, 'Provide valid email address');
+    return setError(emailInput, "Provide valid email address");
   }
 
   //PASSWORD
-  if (passwordInput.value.trim() == '') {
-    return setError(passwordInput, 'Password can not be empty');
-  } else if (passwordInput.value.trim().length < 6 || passwordInput.value.trim().length > 20) {
-    return setError(passwordInput, 'Password min 6 max 20 charecters');
+  if (passwordInput.value.trim() == "") {
+    return setError(passwordInput, "Password can not be empty");
+  } else if (
+    passwordInput.value.trim().length < 6 ||
+    passwordInput.value.trim().length > 20
+  ) {
+    return setError(passwordInput, "Password min 6 max 20 charecters");
   } else {
     return setSuccess(passwordInput);
   }
+}
 
+function canLogin() {
+  let users = localStorage.getItem("users")
+    ? JSON.parse(localStorage.getItem("users"))
+    : [];
 
-  function setError(element, errorMessage) {
-    const parent = element.parentElement;
-    if (parent.classList.contains('success')) {
-      parent.classList.remove('success');
-    }
-    parent.classList.add('error');
-    const paragraph = parent.querySelector('p');
-    paragraph.textContent = errorMessage;
+  users.find(search);
+}
+
+function search(item) {
+  let userEmail = emailInput.value.trim();
+  let userPassword = passwordInput.value.trim();
+  if (userEmail == item.email && userPassword == item.password1) {
+    console.log("im here");
+    location.replace("todo.html");
+    return setSuccess(passwordInput);
+    return true;
+  } else {
+    console.log("password wrong");
+    return setError(passwordInput, "Wrong password or email");
   }
+}
 
-  function setSuccess(element) {
-    const parent = element.parentElement;
-    if (parent.classList.contains('error')) {
-      parent.classList.remove('error');
-    }
-    parent.classList.add('success');
+function setError(element, errorMessage) {
+  const parent = element.parentElement;
+  if (parent.classList.contains("success")) {
+    parent.classList.remove("success");
   }
+  parent.classList.add("error");
+  const paragraph = parent.querySelector("p");
+  paragraph.textContent = errorMessage;
+  return false;
+}
 
-  function isEmailValid(email) {
-    const reg = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-
-    return reg.test(email);
+function setSuccess(element) {
+  const parent = element.parentElement;
+  if (parent.classList.contains("error")) {
+    parent.classList.remove("error");
   }
+  parent.classList.add("success");
+  return true;
+}
+
+function isEmailValid(email) {
+  const reg =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+  return reg.test(email);
 }
