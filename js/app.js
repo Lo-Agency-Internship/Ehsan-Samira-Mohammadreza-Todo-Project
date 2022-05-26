28e219aaf4b232b9020a64c04807bb2f066d3601
samikia <kia.sami8@gmail.com>
2022-05-26 12:12:34 +0430

    filterin date

--- js/app.js
+++ js/app.js
@@ -8,106 +8,66 @@ const clearButton = document.querySelector('#clear-list');//a button that when w
 
 
 
-let todo;
-// const handleItem = function(itemName){
+let todo = [];
 function handleItem(todoitemsssss) {
     const items = itemList.querySelectorAll('.item');
-    // console.log (todo);
+    // console.log(items)
+    items.forEach(function (item) {
 
-    items.forEach(function(item) {
-        // if(item.querySelector('.item-name').textContent === todoitemsssss.title)
-        // {
-                item.querySelector('.complete-item').addEventListener('click', function(){
-                item.querySelector('.item-name').classList.toggle('completed');
-                this.classList.toggle('visibility');
-                todoitemsssss.iscm = true
-            });
+        item.querySelector('.complete-item').addEventListener('click', function () {
+            item.querySelector('.item-name').classList.toggle('completed');
+            this.classList.toggle('visibility');
+            todoitemsssss[item.id].iscm = true;
+            localStorage.setItem('alltodoItems', JSON.stringify(todoitemsssss));
+            getLocalStorage();
+        });
 
-            
-            //edit event listener
-            // item.querySelector('.edit-item').addEventListener('click', function(){
-            //     itemInput.value = itemName;
-            //     itemList.removeChild(item);
-
-            //     todoItems = todoItems.filter(function(item){
-            //         return item !== itemName;
-
-            //     });
-            // });
-
-
-
-        // }
-
-        
+        //edit event listener
+        item.querySelector('.edit-item').addEventListener('click', function () {
+            itemtaskInput.value = todoitemsssss[item.id - 1].title;
+            itemList.removeChild(item);
+            todoitemsssss[item.id-1].title = itemtaskInput.value
+            localStorage.setItem('alltodoItems', JSON.stringify(todoitemsssss));
+            const eee = itemList.querySelectorAll('.item');
+            eee.forEach(function(aaa) {
+                itemList.removeChild(aaa)
+            })
+            getList(todo)
+        });
+       
     })
 }
-   
-//     console.log(items)
-//     items.forEach(function(item){
 
-  
-//             // delete event listener
-//             item.querySelector('.delete-item').addEventListener('click', function(){
-//                 itemList.removeChild(item);
-//                 timeItems = timeItems.filter(function(item){
-//                     return item !== itemName ;
-//                 });
-//                 todoItems = todoItems.filter(function(item){
-//                     return item !== itemName;
-//                 });
-
-//             })
-//         }
-//     })
+// delete event listener
+// item.querySelector('.delete-item').addEventListener('click', function(){
+//     itemList.removeChild(item);
+//     todoitemsssss = todoitemsssss.filter(function(item){
+//         return item !== todo ;
+//     });
+// })
 // }
 
 
-
-
-// const removeItem = function (item) {
-//     // console.log(item);
-//     const removeIndex = (todoItems.indexOf(item));
-//     // const removeTime = (timeItems.indexOf(item));
-
-//     // console.log(removeIndex);
-
-//     todoItems.splice(removeIndex, 1);
-//     // timeItems.splice(removeTime, 1);
-
-
-// }
-
-
-
-
-
-
+const removeItem = function (item) {
+    const removeIndex = (todo.indexOf(item));
+    console.log(todo.indexOf(item))
+    todo.splice(removeIndex, 1);
+}
 
 
 //get data from getlocalstorage and show in client
 const getList = function (todoItems) {
     itemList.innerHTML = '';
     todoItems.forEach(task => {
-     
-        itemList.insertAdjacentHTML('beforeend', `<div class="item my-3"><div class="right-part"><h5 class="item-name text-capitalize" style="fony-size=30px">${task.title}</h5> <h6 class="item-name text-capitalize">${task.time}</h6></div><div class="item-icons"><a href="#" class="complete-item mx-2 item-icon"><i class="far fa-check-circle"></i></a><a href="#" class="edit-item mx-2 item-icon"><i class="far fa-edit"></i></a><a href="#" class="delete-item item-icon"><i class="far fa-times-circle"></i></a></div></div>`);
-        
+        itemList.insertAdjacentHTML('beforeend', `<div class="item my-3" id=${task.taskId}><div class="right-part"><h5 class="item-name text-capitalize" style="fony-size=30px">${task.title}</h5> <h6 class="item-name text-capitalize">${task.time}</h6></div><div class="item-icons"><a href="#" class="complete-item mx-2 item-icon"><i class="far fa-check-circle"></i></a><a href="#" class="edit-item mx-2 item-icon"><i class="far fa-edit"></i></a><a href="#" class="delete-item item-icon"><i class="far fa-times-circle"></i></a></div></div>`);
     });
-
     handleItem(todoItems)
 }
 
 
 
 
-
-
-
-
-
-
 //get our data from local storage and send into getline function 
-
 const getLocalStorage = function () {
     const allitems = localStorage.getItem('alltodoItems')
 
@@ -115,15 +75,17 @@ const getLocalStorage = function () {
         todo = [];
 
     } else {
-        // console.log(allitems)
-        getList(JSON.parse(allitems));
-         todo = JSON.parse(allitems)
+        for (let index = 0; index < todo.length; index++) {
+            if (todo[index].iscm === false) {
+                getList(JSON.parse(allitems).filter(i => (i.iscm !== true)))
+                // console.log(JSON.parse(allitems))
+                todo = JSON.parse(allitems).filter(i => (i.iscm !== true))
+            }
+
+        }
+
     }
 }
-
-
-
-
 
 
 
@@ -133,28 +95,16 @@ const setLocalStorage = function (todoItems) {
 }
 
 // get local storage from page
- getLocalStorage();
-
-
-
-
-
-
-
-
-
+getLocalStorage();
+getList(todo)
 
 //add an item to the List, including to local storage
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     const itemTask = itemtaskInput.value;
     const itemTime = itemtimeinput.value;
-  
-
-
     getLocalStorage();
-    // console.log(todo)
-    let id = todo.length > 0 ? todo[todo.length-1].taskId : 0
+    let id = todo.length > 0 ? todo[todo.length - 1].taskId : 0
     id++;
     // when send empty input we will have a massage
     if (itemTask.length === 0) {
@@ -165,38 +115,158 @@ form.addEventListener('submit', function (e) {
                 feedback.classList.remove('showItem');
             }, 3000);
     }
-
-    else{
-
+    else {
         todo.push(
             {
                 title: itemTask,
-                time : itemTime,
-                taskId : id, 
-                iscm : false
+                time: itemTime,
+                taskId: id,
+                iscm: false
             }
         )
-        // console.log(todo)
         setLocalStorage(todo);
         getList(todo)
         //add event listeners to icons;
     }
-
     itemTask.value = '';
     itemTime.value = '';
 });
 
 
 
-
-
-
-
 // clear all items from the list
 clearButton.addEventListener('click', function () {
-  todo = []
+    todo = []
     localStorage.clear();
     getList(todo);
 
 })
+
+
+
+
+function handldaily() {
+    const d = new Date();
+    let day = d.getDate();
+    let month = d.getMonth();
+    const items = itemList.querySelectorAll('.item');
+    items.forEach(function (item) {
+        itemList.removeChild(item)
+    })
+       let tododate = [] ;
+        for (let index = 0; index < todo.length; index++) {
+            let editdate = todo[index].time
+            let editmonth =editdate.slice(6,7)
+            let editmonthitem = parseInt(editmonth)
+            let dayitem=editdate.slice(8,10)
+            let editdayitem = parseInt(dayitem)
+            if (editmonthitem === month+1) {
+                if (editdayitem === day) {
+                    tododate.push(todo[index])
+                }
+            }
+        }
+        getList(tododate)
+    }
+
+
+
+function handlweekly() {
+    const d = new Date();
+    let day = d.getDate();
+    let month = d.getMonth();
+    let endday = day + 7;
+    let endmonth = month + 1
+    if (endday > 31) { 
+     endmonth = month + 1
+    }
+    const items = itemList.querySelectorAll('.item');
+    items.forEach(function (item) {
+        itemList.removeChild(item)
+    })
+    let tododate = [] ;
+    for (let index = 0; index < todo.length; index++) {
+        let editdate = todo[index].time
+        let editmonth =editdate.slice(6,7)
+        let editmonthitem = parseInt(editmonth)
+        let dayitem=editdate.slice(8,10)
+        let editdayitem = parseInt(dayitem)
+        if (editdayitem>=1 && editdayitem<=8) {
+            editdayitem  = editdayitem +31
+        }
+
+        console.log(day)
+        console.log(month)
+        console.log(endday)
+        console.log(endmonth)
+        console.log(editmonthitem)
+        console.log(editdayitem)
+        if (editmonthitem === month+1 || editmonthitem === endmonth + 1) {
+            if (editmonthitem === endmonth + 1) {
+                if (editdayitem>31 && editdayitem<38) {
+                    if (editdayitem >= day && editdayitem<=endday) {
+                        tododate.push(todo[index])
+                        console.log(tododate)
+                    }
+                }
+            }
+            else
+            {
+                if (editdayitem >= day && editdayitem<=endday) {
+                    tododate.push(todo[index])
+                    console.log(tododate)
+                }
+            }
+            
+        }
+    }
+    getList(tododate)
+}
+
+
+
+
+
+function handlmonthly() {
+    const d = new Date();
+    let month = d.getMonth();
+    const items = itemList.querySelectorAll('.item');
+    items.forEach(function (item) {
+        itemList.removeChild(item)
+    })
+
+    let tododate = [] ;
+    for (let index = 0; index < todo.length; index++) {
+        let editdate = todo[index].time
+        let editmonth =editdate.slice(6,7)
+        let editmonthitem = parseInt(editmonth)
+        if (editmonthitem === month+1) {
+            tododate.push(todo[index])
+        }
+    }
+    getList(tododate)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
